84cc99cef352a52a4c7d422d6f555f99513b0045
jackp <none@example.com>
2013-12-19 01:17:45 +0000

    Wed Dec 18 14:36:30 PST 2013 jackp Checking in Epix.

--- ana/constituents.mk
+++ ana/constituents.mk
@@ -5,5 +5,5 @@ tgtlibs_timetool := pds/utility pds/collection pds/service pds/vmon pds/mon pds/
 tgtlibs_timetool += timetool/ttevent timetool/ttsvc
 tgtlibs_timetool += pdsdata/xtcdata pdsdata/anadata pdsdata/compressdata pdsdata/indexdata pdsdata/psddl_pdsdata pdsalg/pdsalg
 tgtslib_timetool := ${USRLIBDIR}/rt ${USRLIBDIR}/pthread 
-tgtincs_timetool := pdsdata/include pdsalg/include 
+tgtincs_timetool := pdsdata/include pdsalg/include boost/include ndarray/include 
 
