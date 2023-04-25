c2a7b81c9bb013940dc07f103c03587cc08214d0
Daniel Damiani <ddamiani@slac.stanford.edu>
2023-04-25 00:35:43 -0700

    Fix problem in makefiles so FrameCache.hh header is included

--- service/constituents.mk
+++ service/constituents.mk
@@ -5,11 +5,12 @@ libincs_ttsvc := pdsdata/include
 libincs_ttsvc += psalg/include ndarray/include boost/include
 libincs_ttsvc += gsl/include
 
-special_include_files := Fex.hh
+special_include_files := Fex.hh FrameCache.hh
+special_include_files := $(patsubst %,$(RELEASE_DIR)/build/timetool/include/timetool/service/%,$(special_include_files))
 
-userall: $(RELEASE_DIR)/build/timetool/include/timetool/service/$(special_include_files);
+userall: $(special_include_files)
 
-$(RELEASE_DIR)/build/timetool/include/timetool/service/$(special_include_files): $(special_include_files)
+$(RELEASE_DIR)/build/timetool/include/timetool/service/%.hh: %.hh
 	@echo "[I] Target <include>";
 	$(quiet)mkdir -p $(RELEASE_DIR)/build/timetool/include/timetool/service
 	$(quiet)cp $< $@
